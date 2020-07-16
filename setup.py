2b8fefaf9115d875364719f8038aa9a653062a0c
John Hemmick <33430538+hemmick@users.noreply.github.com>
2020-07-16 10:43:19 -0500

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+        name="hcapy-hemmick",
+        version="0.0.1",
+        author="hemmick",
+        author_email="john.hemmick@silabs.com",
+        description="HCA(Py) the z-ware hcapi wrapped in python",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/hemmick/hcapy",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6'
+)
